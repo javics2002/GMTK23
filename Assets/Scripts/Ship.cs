c4163ca684c4cb4f7a18848e4aa2f8a14ffe1ce3
DiegoRol69 <drol@ucm.es>
2023-07-07 20:58:04 +0200

    comenzamos con la jamm :)

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int lifes;
+    public float speed;
+    public float shootColdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShipsGenerator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipsGenerator : MonoBehaviour
+{
+    public GameObject bossShip;
+
+    public GameObject [] enemiesShips;
+
+    public GameObject playerShip;
+
+    public int colums;
+    public int rows;
+
+    public float spaceWidth;
+    public float spaceHeight;
+
+    public float separation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 bossShipPos = new Vector3(spaceWidth / 2, 0, spaceHeight);
+        Vector3 playerShipPos = new Vector3(spaceWidth / 2, 0, 0);
+
+        Instantiate(bossShip, bossShipPos, Quaternion.identity);
+        Instantiate(playerShip, playerShipPos, Quaternion.identity);
+
+        Vector3 enemyShipPos;
+
+        int[] patronEnemies = new int[5];
+        patronEnemies[0] = 2;
+        patronEnemies[1] = 1;
+        patronEnemies[2] = 1;
+        patronEnemies[3] = 0;
+        patronEnemies[4] = 0;
+
+        for (int i = 0; i < rows; i++){
+
+            for(int j = 0; j < colums; j++)
+            {
+                enemyShipPos = new Vector3(5 + j*10, 0, spaceHeight - 10 - i*10);
+                Instantiate(enemiesShips[patronEnemies[i]], enemyShipPos, Quaternion.identity);
+
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
